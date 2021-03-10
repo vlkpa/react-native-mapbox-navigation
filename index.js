bfccfe3c8405eacdee495670b60570359b5e6020
vlkpa <vlk.pata@gmail.com>
2021-03-10 09:54:53 +0100

    added configurable custom url

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ MapboxNavigation.propTypes = {
   onCancelNavigation: PropTypes.func,
   onArrive: PropTypes.func,
   showsEndOfRouteFeedback: PropTypes.bool,
+  customDirectionsUrl: PropTypes.string,
 };
 
 const RNMapboxNavigation = requireNativeComponent(
